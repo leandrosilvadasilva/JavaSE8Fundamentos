dcffbe3dca2e5e199c8be35c479f5fd03bf8f925
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2020-12-15 15:45:11 -0300

    Primeiras classes

---
+++ JavaSE8Fundamentos/src/br/com/leandro/variaveis/Exercicios01.java
@@ -0,0 +1,5 @@
+package br.com.leandro.variaveis;
+
+public class Exercicios01 {
+
+}
---
+++ JavaSE8Fundamentos/src/br/com/leandro/variaveis/IntroVariaveis.java
@@ -0,0 +1,20 @@
+package br.com.leandro.variaveis;
+
+public class IntroVariaveis {
+	
+	public static void main(String[] args) {
+		
+		//Objetivo: Entendimento das variáveis em Java
+		// inspirado no cusro de Loiane Groner
+		//https://www.youtube.com/watch?v=ELBqT_rueAQ&feature=emb_logo
+		
+		int idade;
+		String nome;
+		String nomeDoMeuCachorro;
+		
+		
+		
+		
+	}
+
+}

c14e140312c3b4f9182771a84296e320427c238d
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2020-12-20 20:00:29 -0300

    Mais exercícios de controle

---
+++ JavaSE8Fundamentos/src/br/com/leandro/controles/exercicios/Exercicio06.java
@@ -0,0 +1,27 @@
+package br.com.leandro.controles.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+	public static void main(String[] args) {
+		/*
+		 * 6. Faça um Programa que leia três números e mostre o maior deles.
+		 * 
+		 */
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite 3 números");
+		double num1 = scan.nextDouble();
+		double num2 = scan.nextDouble();
+		double num3 = scan.nextDouble();
+		
+		if (num1 > num2 && num1 > num3) {
+			System.out.println("Número 1 é maior: " + num1);
+		} else if (num2 > num1 && num2 > num3) {
+			System.out.println("Número 2 é maior: " + num2);
+		}else {
+			System.out.println("Número 3 é maior: " + num3);
+		}
+		scan.close();
+	}
+}
